1f4277f0816db36dc6053a63017bf276b404a1df
Manohar0450 <nskmanohar242005@gmail.com>
2024-04-22 09:34:23 +0530

    mame changed

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
 
 
-          <div className="text-sm text-gray-600 mr-4">&copy; Designed And Developed By Nvision It Team.
+          <div className="text-sm text-gray-600 mr-4">&copy; Designed And Developed By Manohar.
 
         </div>
 
