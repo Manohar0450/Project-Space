53be4fb159d1cd335cf694ec6049494e2c5678c5
Manohar0450 <nskmanohar242005@gmail.com>
2024-04-22 13:55:28 +0530

    gbdfgdf

--- components/resourcesLinks.tsx
+++ components/resourcesLinks.tsx
@@ -1,32 +1,31 @@
 import React from 'react';
 import Image from 'next/image';
- 
+
 export default function ResourcesLinks() {
   return (
     <section className="relative" style={{ backgroundColor: "#66FF00" }}>
       <div className="container mx-auto mt-12 px-4">
         <div className="rounded-lg overflow-hidden shadow-lg bg-gradient-to-r ">
           <div className="p-8">
-          <button className="container flex justify-center items-center button-red mb-8">Ready To Get Started</button>
+            <button className="container flex justify-center items-center button-red mb-8">Ready To Get Started</button>
 
-            <h2 className="text-2xl font-bold text-white mb-4">Whether you're a project seeker in search of the perfect collaboration or a creator eager to embark on exciting new projects, Project Connect is your gateway to success. Join our community today and unlock endless possibilities!</h2>
+            <h2 className="text-2xl font-bold text-white mb-4">Whether you're a project seeker in search of the perfect collaboration or a creator eager to embark on exciting new projects, Project Connect is your gateway to success. Join our community today and unlock endless possibilities!</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {/* Skin Cancer Foundation */}
+              {/* Email Address */}
               <ResourceCard
                 title="Email Address"
                 links={[
-                  { text: "projectconnect.pc@gmail.com", url: "https://in.search.yahoo.com/search?fr=mcafee&type=E211IN826G0&p=gmail" },
+                  { text: "projectconnect.pc@gmail.com", url: "mailto:projectconnect.pc@gmail.com" },
                 ]}
               />
-              {/* American Academy of Dermatology */}
+              {/* WhatsApp */}
               <ResourceCard 
-                 title="Whats App"
+                title="WhatsApp"
                 links={[
                   { text:"9010488777", url: "" },
-
-                  ]}
+                ]}
               />
-              {/* Melanoma Research Fund (MRF) */}
+              {/* Any other queries Contact */}
               <ResourceCard
                 title="Any other queries Contact"
                 links={[
@@ -48,7 +47,7 @@ const ResourceCard = ({ title, links }: { title: string; links: { text: string; 
       <ul>
         {links.map((link, index) => (
           <li key={index} className="mb-2">
-            <a href={link.url} className="text-orange-700 hover:underline">{link.text}</a>
+            <a href={link.url} className="text-orange-700 hover:underline" target="_blank" rel="noopener noreferrer">{link.text}</a>
           </li>
         ))}
       </ul>
